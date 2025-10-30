5da1fa79bd4fe4f0a8609beb0dd7c57855233742
XxcrygirlxX <elsuasnavassi@uide.edu.ec>
2025-10-30 12:22:17 -0500

    Update xor_cipher.py

--- xor_cipher.py
+++ xor_cipher.py
@@ -1,14 +1,11 @@
 import random
 
 def xor (mensaje, llave):
-    return ''.join(chr(ord(m) ^ ord(l)) for m, l in zip(mensaje, llave))
+    return ''.join(chr(ord(mensaje[i]) ^ ord(llave[i])) for i in range(len(mensaje)))
 
 mensaje = "buenos dias mundo"
-llave = ''.join(str(random.randint(0, 1)) for _ in range(len(mensaje)))
+llave = ''.join(str(random.randint(0, 1)) for _ in mensaje)
 cifrado = xor(mensaje, llave)
 descifrado = xor(cifrado, llave)
 
-print("Mensaje original:", mensaje)
-print("Llave:", llave)  
-print("Mensaje cifrado:", cifrado)
-print("Mensaje descifrado:", descifrado)
+print(f"Mensaje original:", mensaje, "\nLlave:", llave, "\nMensaje cifrado:", cifrado, "\nMensaje descifrado:", descifrado)
